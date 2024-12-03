cbc5580b4739301bd96b895996f7f483d7855c54
AlleyTerry <art8689@nyu.edu>
2024-12-03 10:44:00 -0500

    color hover script

--- MusicFestForever/Assets/Scripts/SceneHandler.cs
+++ MusicFestForever/Assets/Scripts/SceneHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,9 @@ public class SceneHandler : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
