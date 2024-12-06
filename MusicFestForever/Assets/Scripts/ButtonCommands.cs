b010d47926f6f241bd1ddcc917debdeaa93977d9
segaln17 <segaln17@gmail.com>
2024-12-06 01:33:43 -0500

    new part of Shoelace node for contraband box, new script for ending, sprite and button fixes
    new node and new script aren't connected yet but they're transcribed; haven't gotten to test yet

--- MusicFestForever/Assets/Scripts/ButtonCommands.cs
+++ MusicFestForever/Assets/Scripts/ButtonCommands.cs
@@ -12,8 +12,8 @@ public class ButtonCommands : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        leftButton.gameObject.SetActive(false);
-        rightButton.gameObject.SetActive(false);
+        leftButton.gameObject.SetActive(true);
+        rightButton.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
