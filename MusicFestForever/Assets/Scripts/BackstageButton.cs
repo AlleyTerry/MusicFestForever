e35a86d3477ca2926d4e7efb568274663d86f60a
segaln17 <segaln17@gmail.com>
2024-12-18 13:07:56 -0500

    button deactivation

---
+++ MusicFestForever/Assets/Scripts/BackstageButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackstageButton : MonoBehaviour
+{
+    public GameObject backstageButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backstageButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
